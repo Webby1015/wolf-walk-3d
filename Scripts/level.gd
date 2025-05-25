37d8f0314712a08336f61e655cf21afa61990eed
webby1015 <vedantabhishek@gmail.com>
2025-05-26 02:11:03 +0530

    map vegitation complete

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -4,9 +4,11 @@ extends Node3D
 var paused = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	print(Engine.get_frames_per_second())
 	pass
